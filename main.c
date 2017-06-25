d323b2eadfcb0d625f6f11f2e89c1c94468f88a6
Kevin Charm <kevin@kevincharm.com>
2017-06-25 15:25:43 +0800

    debugging

--- main.c
+++ main.c
@@ -26,34 +26,49 @@
  *  chat pointer is then + 52
  *  @returns mem addr offset of chatlog
  */
-#define MEMSCAN_BUFFER_SIZE (0x1000)
+#define MEMSCAN_BUFFER_SIZE (0x100000)
 int get_chatlog_offset(mach_port_t task) {
-    static uint16_t MEM_PATTERN_CHATLOG[] = {0xe8, 0xffff, 0xffff, 0xffff, 0xffff, 0x85, 0xc0,
+    /*static uint16_t MEM_PATTERN_CHATLOG[] = {0xe8, 0xffff, 0xffff, 0xffff, 0xffff, 0x85, 0xc0,
         0x74, 0x0e, 0x48, 0x8b, 0x0d, 0xffff, 0xffff, 0xffff, 0xffff, 0x33, 0xd2, 0xe8, 0xffff,
-        0xffff, 0xffff, 0xffff,0x48, 0x8b, 0x0d};
+        0xffff, 0xffff, 0xffff,0x48, 0x8b, 0x0d};*/
+    static uint8_t MEM_PATTERN_CHATLOG[] = {0x3A, 0x3A, 0x57, 0x65, 0x6C, 0x63, 0x6F, 0x6D,
+                                            0x65, 0x20, 0x74, 0x6F, 0x20, 0x46, 0x49, 0x4E,
+                                            0x41, 0x4C, 0x20, 0x46, 0x41, 0x4E, 0x54, 0x41,
+                                            0x53, 0x59, 0x20, 0x58, 0x49, 0x56, 0x21, 0x9A,
+                                            0x1A, 0x4F, 0x59, 0x03, 0x00, 0x00, 0x00, 0x3A,
+                                            0x3A};
+                                            // "::Welcome to FINAL FANTASY XIV!OY"
+    static unsigned char MEM_PATTERN_TEST[] = "";
     int sigsize = sizeof(MEM_PATTERN_CHATLOG)/sizeof(MEM_PATTERN_CHATLOG[0]);
 
-    unsigned int size = 0x01000000; // size of memory to scan
+    unsigned int size = 0xffff3000; // size of memory to scan
 
     int bytes_read = 0;
     uint32_t sz;
     printf("Getting chatlog memory offset...\n");
     while (bytes_read <= size) {
         static uint8_t buffer[MEMSCAN_BUFFER_SIZE];
-        unsigned int address = 0x0107E3F0+bytes_read;
+        unsigned int address = /*0x0107E3F0+*/bytes_read;
         pointer_t buf_ptr;
 
         // vm_read
         //printf("\nStarting vm_read...\n");
         kern_return_t kret = vm_read(task, address, MEMSCAN_BUFFER_SIZE, &buf_ptr, &sz);
-        KERN_ERR_CHECK(kret, "vm_read");
-        if (kret != KERN_SUCCESS) return -1;
+        if (kret == 2) {
+            bytes_read+=MEMSCAN_BUFFER_SIZE;
+            continue;
+        } else if (kret != KERN_SUCCESS) {
+            KERN_ERR_CHECK(kret, "vm_read");
+            return -1;
+        }
 
         //printf("Finished vm_read. buffer pointer: %d\n", buf_ptr);
+        memset(buffer, 0, MEMSCAN_BUFFER_SIZE);
         memcpy(buffer, (const void *)buf_ptr, MEMSCAN_BUFFER_SIZE); // why is this segfaulting?
+        vm_deallocate(task, buf_ptr, sz); // vm_read returns mallocd memory that needs to be freed
         //printf("Finished memcpy.\n");
         unsigned int bufpos = 0;
-        //printf("Reading 1MB memory at: %x\n", address);
+        printf("Reading 1MB memory at: %x\n", address);
         while (bufpos <= MEMSCAN_BUFFER_SIZE) {
             unsigned int sigstart = bufpos;
             unsigned int sigpos = 0;
@@ -66,6 +81,9 @@ int get_chatlog_offset(mach_port_t task) {
                     return (int)(bytes_read+bufpos);
                 }
             }
+            if (sigpos > 0)
+                printf("\nPattern incomplete!\n");
+
             printf("\33[2K\r\x1B[33;1mSearching...\x1B[0m");
             bufpos++;
         }
@@ -73,6 +91,23 @@ int get_chatlog_offset(mach_port_t task) {
     }
 
     return -1;
+}
+
+#define CHATLOG_POINTER         (0x2CA90E58)
+#define CHATLOG_READ_SIZE       (0x00100000)
+void extract_chatlog_from_static_offset(mach_port_t task) {
+    static uint8_t buffer[CHATLOG_READ_SIZE];
+    uint32_t sz;
+    pointer_t buf_ptr;
+    kern_return_t kret = vm_read(task, CHATLOG_POINTER, CHATLOG_READ_SIZE, &buf_ptr, &sz);
+    KERN_ERR_CHECK(kret, "vm_read");
+
+    printf("Finished vm_read. buffer pointer: %d\n", buf_ptr);
+    memset(buffer, 0, CHATLOG_READ_SIZE);
+    memcpy(buffer, (const void *)buf_ptr, CHATLOG_READ_SIZE);
+    vm_deallocate(task, buf_ptr, sz);
+
+    for (int i=0; i<sz; i++) printf("%c", (char)buffer[i]);
 }
 
 /**
@@ -121,6 +156,8 @@ int main() {
 
     printf("\nFFXIV (%d) is running %d threads.\n", pid_ffxiv, thread_count);
     printf("  -> CHATLOG at %d\n", chatlog_addr);
+
+    //extract_chatlog_from_static_offset(task);
 
     return 0;
 }
