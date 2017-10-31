81bc12912929ac8bd6c3807587a4e633a1f530a3
Kevin Charm <kevin@kevincharm.com>
2017-10-31 17:59:10 +0800

    remove old pcap code

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// old
-/*
-const pcap = require('pcap2')
-const tcpTracker = new pcap.TCPTracker()
-function openPcap(opts) {
-    const { ports, filter } = opts
-    console.log(`pcap filter: ${filter}`)
-    const pcapSession = new pcap.Session('en0', { filter })
-
-    tcpTracker.on('session', session => {
-        const { dst_name, src_name } = session
-        console.log(`Begin TCP session ${src_name}->${dst_name}`)
-
-        session.on('data recv', onTcpPacket.bind({ ports }))
-
-        session.on('end', session => {
-            console.log('End TCP session')
-        })
-    })
-
-    pcapSession.on('packet', raw => {
-        const packet = pcap.decode.packet(raw)
-        tcpTracker.track_packet(packet)
-    })
-}
-*/
-
 const { Cap, decoders } = require('cap')
 const { PROTOCOL } = decoders
 const { getFfxivProcessInfo } = require('./lib/process')
